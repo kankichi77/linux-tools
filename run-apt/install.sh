ff5d86d170a76c3633385b4df99c1012bfc9589f
kan <github@kanbot.xyz>
2023-01-14 16:36:23 +0800

    work

--- run-apt/install.sh
+++ run-apt/install.sh
@@ -1,7 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 # script to install a bash script that will run "apt-get update" and "apt-get upgrade -y" at once
+# run this script with the "source"/"." command, like so: ". ./install.sh"
 
-mkdir -p $HOME/bin
-echo "sudo apt-get update && sudo apt-get upgrade -y" > $HOME/bin/run-apt.sh
-chmod 744 $HOME/bin/run-apt.sh
+bindir=$HOME/.local/bin
+
+mkdir -p $bindir
+echo "sudo apt-get update && sudo apt-get upgrade -y" > $bindir/run-apt.sh
+chmod 744 $bindir/run-apt.sh
 . $HOME/.profile
+echo "run-apt.sh script installed $bindir"
